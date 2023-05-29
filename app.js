78f363a6cbc74be5c984713dc614ccc8228e80f8
Tung Ngo <tungntgch220075.fpt.edu.vn>
2023-05-29 14:21:19 +0700

    ok

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Cài đặt và cấu hình mongoose
+var mongoose = require('mongoose');
+var uri = "mongodb://127.0.0.1:27017/book";
+mongoose.connect(uri)
+  .then(() => { console.log('connect to DB success!!') })
+  .catch((err) => { console.error(err) });
+
+// Cài đặt và cấu hình body-parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,12 +34,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +48,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Set cổng cho server 
+var port = 3001;
+app.listen(port);
 
 module.exports = app;
