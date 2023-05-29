2de726aa971575b29ae012bafa28703574fa1f23
Tung Ngo <tungntgch220075.fpt.edu.vn>
2023-05-29 14:00:18 +0700

    ok

---
+++ data.js
@@ -0,0 +1,39 @@
+[
+    {
+        "title": "NodeJS Basic",
+        "genre": "Programming",
+        "author": "David",
+        "quantity": 10,
+        "price": 19.99,
+        "date": "2022-01-25",
+        "cover": "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/yellow-business-leadership-book-cover-design-template-dce2f5568638ad4643ccb9e725e5d6ff.jpg?ts=1637017516"
+    },
+    {
+        "title": "Creative Ideas",
+        "genre": "Novel",
+        "author": "Claudia",
+
+        "quantity": 15,
+        "price": 15.68,
+       "date": "2023-05-12",
+        "cover": "https://edit.org/images/cat/book-covers-big-2019101610.jpg"
+    },
+    {
+        "title": "Our Last Summer",
+        "genre": "Education",
+        "author": "James",
+        "quantity": 8,
+        "price": 20.66,
+        "date": "2023-02-07", 
+        "cover": "https://marketplace.canva.com/EAFA7N_NLQs/1/0/1003w/canva-cute-colorful-watercolor-simple-illustrated-young-adult-romance-book-cover-ooKN90UU-H0.jpg"
+    },
+    {
+        "title": "Normal People",
+        "genre": "Novel",
+        "author": "Sally Rooney",
+        "quantity": 22,
+        "price": 22.99,
+        "date": "2023-05-12",
+        "cover": "https://cdn.vox-cdn.com/thumbor/p-gGrwlaU4rLikEAgYhupMUhIJc=/0x0:1650x2475/1200x0/filters:focal(0x0:1650x2475):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/13757614/817BsplxI9L.jpg"
+    }
+]
